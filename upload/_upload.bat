60ea61481f11bd354baca0dec94b2a9a2beaf6a8
Logiqx <software@mikeg.me.uk>
2005-02-24 23:20:07 +0000

    Updated guides to use MSYS

--- upload/_upload.bat
+++ upload/_upload.bat
@@ -1,25 +1,11 @@
 @ECHO OFF
 
-ECHO Local archive:
+ECHO Uploading to %LOGIQX_LOGIN%@%LOGIQX_FTP%...
 ECHO.
 
-ls -l  all.tar.gz
+pscp all.tar.gz %LOGIQX_LOGIN%@%LOGIQX_FTP%:web/upload/all.tar.gz
 
-ECHO.
-ECHO Transferring 'all.tar.gz' to %LOGIQX_FTP%/web/upload...
-
-ECHO open %LOGIQX_FTP%       >_upload.ftp
-ECHO user %LOGIQX_LOGIN%    >>_upload.ftp
-ECHO cd web/upload          >>_upload.ftp
-ECHO bin                    >>_upload.ftp
-ECHO put all.tar.gz         >>_upload.ftp
-ECHO dir all.tar.gz         >>_upload.ftp
-ECHO bye                    >>_upload.ftp
-
-ECHO.
-FTP -i -n <_upload.ftp
-
-rm -f _upload.ftp all.tar.gz
+rm -f all.tar.gz
 
 ECHO.
 PAUSE
