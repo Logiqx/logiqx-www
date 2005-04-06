33cfe97793d5d178e1cef6e5b17518bf335418c6
Logiqx <software@mikeg.me.uk>
2005-04-06 21:45:48 +0000

    New server

--- upload/_upload.bat
+++ upload/_upload.bat
@@ -3,7 +3,7 @@
 ECHO Uploading to %LOGIQX_LOGIN%@%LOGIQX_FTP%...
 ECHO.
 
-pscp all.tar.gz %LOGIQX_LOGIN%@%LOGIQX_FTP%:web/upload/all.tar.gz
+pscp all.tar.gz %LOGIQX_LOGIN%@%LOGIQX_FTP%:www/upload/all.tar.gz
 
 rm -f all.tar.gz
 
