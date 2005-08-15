4753305505e788c411e1cc83a8ce1fb4024776ae
Logiqx <software@mikeg.me.uk>
2005-08-15 21:37:27 +0000

    Now in Resource (Top.php)

--- ads/cube.php
+++
@@ -1,1 +0,0 @@
-						<a href="http://www.x-arcade.com/landingpage.shtml?kbid=72062&amp;img=banner.gif"><img src="http://xgaming.com/ShowBanner.php?id=72062&amp;img=banner.gif" alt="X-Arcade"/></a>
--- ads/top.php
+++
@@ -1,1 +0,0 @@
-						<a href="http://www.x-arcade.com/landingpage.shtml?kbid=72062"><img src="http://www.xgaming.com/Rotate1.php?id=72062" alt="X-Arcade"/></a>
