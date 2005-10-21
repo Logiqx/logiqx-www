2cfa04d1e8ccb3ee709e54a48e3d0c148acc8d11
Logiqx <software@mikeg.me.uk>
2005-10-21 21:03:12 +0000

    Fixed script error

--- Resources/Top.php
+++ Resources/Top.php
@@ -43,35 +43,35 @@
 						echo INDENT . '<hr />' . LF . LF;
 
 						echo INDENT . '<p><a href="' . $www_root . '"><img src="' . $www_root . 'Menu/NewsOff.gif"' . LF;
-						echo INDENT . TAB . 'onmouseover="menuOn(' . "'" . 'News' . "'" . ' onmouseout="menuOff(' . "'" . 'News' . "'" . ')' . LF;
+						echo INDENT . TAB . 'onmouseover="menuOn(' . "'" . 'News' . "'" . ')" onmouseout="menuOff(' . "'" . 'News' . "'" . ')"' . LF;
 						echo INDENT . TAB . 'alt="News" width="40" height="20" id="News" /></a></p>' . LF . LF;
 
 						echo INDENT . '<p><a href="http://caesar.logiqx.com/"><img src="' . $www_root . 'Menu/CAESAROff.gif"' . LF;
-						echo INDENT . TAB . 'onmouseover="menuOn(' . "'" . 'CAESAR' . "'" . ' onmouseout="menuOff(' . "'" . 'CAESAR' . "'" . ')' . LF;
+						echo INDENT . TAB . 'onmouseover="menuOn(' . "'" . 'CAESAR' . "'" . ')" onmouseout="menuOff(' . "'" . 'CAESAR' . "'" . ')"' . LF;
 						echo INDENT . TAB . 'alt="CAESAR" width="64" height="20" id="CAESAR" /></a></p>' . LF . LF;
 
 						echo INDENT . '<p><a href="http://forum.logiqx.com/"><img src="' . $www_root . 'Menu/ForumOff.gif"' . LF;
-						echo INDENT . TAB . 'onmouseover="menuOn(' . "'" . 'Forum' . "'" . ' onmouseout="menuOff(' . "'" . 'Forum' . "'" . ')' . LF;
+						echo INDENT . TAB . 'onmouseover="menuOn(' . "'" . 'Forum' . "'" . ')" onmouseout="menuOff(' . "'" . 'Forum' . "'" . ')"' . LF;
 						echo INDENT . TAB . 'alt="Forum" width="46" height="20" id="Forum" /></a></p>' . LF . LF;
 
 						echo INDENT . '<p><a href="' . $www_root . 'Dats/"><img src="' . $www_root . 'Menu/DatsOff.gif"' . LF;
-						echo INDENT . TAB . 'onmouseover="menuOn(' . "'" . 'Dats' . "'" . ' onmouseout="menuOff(' . "'" . 'Dats' . "'" . ')' . LF;
+						echo INDENT . TAB . 'onmouseover="menuOn(' . "'" . 'Dats' . "'" . ')" onmouseout="menuOff(' . "'" . 'Dats' . "'" . ')"' . LF;
 						echo INDENT . TAB . 'alt="Dats" width="33" height="20" id="Dats" /></a></p>' . LF . LF;
 
 						echo INDENT . '<p><a href="' . $www_root . 'Tools/"><img src="' . $www_root . 'Menu/ToolsOff.gif"' . LF;
-						echo INDENT . TAB . 'onmouseover="menuOn(' . "'" . 'Tools' . "'" . ' onmouseout="menuOff(' . "'" . 'Tools' . "'" . ')' . LF;
+						echo INDENT . TAB . 'onmouseover="menuOn(' . "'" . 'Tools' . "'" . ')" onmouseout="menuOff(' . "'" . 'Tools' . "'" . ')"' . LF;
 						echo INDENT . TAB . 'alt="Tools" width="40" height="20" id="Tools" /></a></p>' . LF . LF;
 
 						echo INDENT . '<p><a href="' . $www_root . 'DatFAQs/"><img src="' . $www_root . 'Menu/DatFAQsOff.gif"' . LF;
-						echo INDENT . TAB . 'onmouseover="menuOn(' . "'" . 'DatFAQs' . "'" . ' onmouseout="menuOff(' . "'" . 'DatFAQs' . "'" . ')' . LF;
+						echo INDENT . TAB . 'onmouseover="menuOn(' . "'" . 'DatFAQs' . "'" . ')" onmouseout="menuOff(' . "'" . 'DatFAQs' . "'" . ')"' . LF;
 						echo INDENT . TAB . 'alt="DatFAQs" width="69" height="20" id="DatFAQs" /></a></p>' . LF . LF;
 
 						echo INDENT . '<p><a href="' . $www_root . 'HowTo/"><img src="' . $www_root . 'Menu/HowToOff.gif"' . LF;
-						echo INDENT . TAB . 'onmouseover="menuOn(' . "'" . 'HowTo' . "'" . ' onmouseout="menuOff(' . "'" . 'HowTo' . "'" . ')' . LF;
+						echo INDENT . TAB . 'onmouseover="menuOn(' . "'" . 'HowTo' . "'" . ')" onmouseout="menuOff(' . "'" . 'HowTo' . "'" . ')"' . LF;
 						echo INDENT . TAB . 'alt="HowTo" width="116" height="20" id="HowTo" /></a></p>' . LF . LF;
 
 						echo INDENT . '<p><a href="' . $www_root . 'Links/"><img src="' . $www_root . 'Menu/LinksOff.gif"' . LF;
-						echo INDENT . TAB . 'onmouseover="menuOn(' . "'" . 'Links' . "'" . ' onmouseout="menuOff(' . "'" . 'Links' . "'" . ')' . LF;
+						echo INDENT . TAB . 'onmouseover="menuOn(' . "'" . 'Links' . "'" . ')" onmouseout="menuOff(' . "'" . 'Links' . "'" . ')"' . LF;
 						echo INDENT . TAB . 'alt="Links" width="37" height="20" id="Links" /></a></p>' . LF . LF;
 
 						echo INDENT . '<hr />' . LF . LF;
