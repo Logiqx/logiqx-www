d57beb5976c18ab12babb0ec5e0c12b6a2f7465d
Logiqx <software@mikeg.me.uk>
2007-04-08 20:40:59 +0000

    Synthetic commit for incomplete tag WWW-2007-04-08

---
+++ Dats/NebulaJukebox/NebulaJukebox.php
@@ -0,0 +1,352 @@
+
+	
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+  <head>
+    <script language="php">
+					// Standard PHP includes (database connection and constants)
+
+					include ('../../Resources/Include.php');
+
+				</script>
+    <title>Nebula Jukebox v2.9</title>
+    <script language="php">
+					// Include standard &lt;head> metadata
+
+					include('../../Resources/Head.php');
+				</script>
+  </head>
+  <body>
+    <script language="php">
+					// The main page content is a 3 column table (left column is the menu, right one is the news)
+
+					echo '' . LF . LF;
+
+					include('../../Resources/Top.php');
+				</script>
+    <p>
+      <img src="../../Resources/Logiqx.png" width="327" height="68" alt="../../Resources/Logiqx.png"/>
+    </p>
+	<p>
+      <img src="NebulaJukebox.png" width="189" height="40" alt="NebulaJukebox.png"/>
+    </p>
+	<p>249 games for Nebula Jukebox v2.9</p>
+	
+	
+	
+	
+	
+	
+	
+		<p>Neo-Geo, CPS-1, CPS-2, ZN-1, ZN-2, Sega Model 2 and 3 sound player</p>
+	
+	<table>
+      <colgroup span="1" width="50%"/>
+      <colgroup span="1" width="50%"/>
+      <tr>
+		<td>
+          <p>Download <a href="Nebula%20Jukebox%20v2.9%20(cm).zip">Nebula Jukebox dat for CMPro</a> (37KB)</p>
+          <p>
+            <a href="Nebula%20Jukebox%20v2.9%20(cm).zip">
+              <img src="../../Resources/Disk.png" width="38" height="38" alt="Download"/>
+            </a>
+          </p>
+        </td>
+		<td>
+          <p>Download <a href="Nebula%20Jukebox%20v2.9%20(rc).zip">Nebula Jukebox dat for ROMCenter</a> (17KB)</p>
+          <p>
+            <a href="Nebula%20Jukebox%20v2.9%20(rc).zip">
+              <img src="../../Resources/Disk.png" width="38" height="38" alt="Download"/>
+            </a>
+          </p>
+        </td>
+	</tr>
+    </table>
+	<hr/>
+    <p>
+      <img src="../../Resources/History.png" width="85" height="40" alt="History"/>
+    </p>
+    <p>
+      <b>5 January 2007</b> - 
+			
+			Removed MAME supplements.
+		</p>
+    <p>
+      <b>11 December 2006</b> - 
+			
+			No change required for MAME v0.111.
+		</p>
+    <p>
+      <b>19 November 2006</b> - 
+			
+			No change required for MAME v0.110.
+		</p>
+    <p>
+      <b>27 September 2006</b> - 
+			
+			No change required for MAME v0.109.
+		</p>
+    <p>
+      <b>21 August 2006</b> - 
+			
+			No change required for MAME v0.108.
+		</p>
+    <p>
+      <b>24 July 2006</b> - 
+			
+			Updated supplements for MAME v0.107.
+		</p>
+    <p>
+      <b>14 May 2006</b> - 
+			
+			No change required for MAME v0.106.
+		</p>
+    <p>
+      <b>5 April 2006</b> - 
+			
+			No change required for MAME v0.105.
+		</p>
+    <p>
+      <b>6 February 2006</b> - 
+			
+			No change required for MAME v0.104.
+		</p>
+    <p>
+      <b>4 January 2006</b> - 
+			
+			No change required for MAME v0.103.
+		</p>
+    <p>
+      <b>24 November 2005</b> - 
+			
+			No change required for MAME v0.102.
+		</p>
+    <p>
+      <b>10 October 2005</b> - 
+			
+			No change required for MAME v0.101.
+		</p>
+    <p>
+      <b>17 September 2005</b> - 
+			
+			No change required for MAME v0.100.
+		</p>
+    <p>
+      <b>7 August 2005</b> - 
+			
+			No change required for MAME v0.99.
+		</p>
+    <p>
+      <b>15 July 2005</b> - 
+			
+			No change required for MAME v0.98.
+		</p>
+    <p>
+      <b>6 June 2005</b> - 
+			
+			No change required for MAME v0.97.
+		</p>
+    <p>
+      <b>7 May 2005</b> - 
+			
+			No change required for MAME v0.96.
+		</p>
+    <p>
+      <b>28 March 2005</b> - 
+			
+			No change required for MAME v0.95.
+		</p>
+    <p>
+      <b>6 March 2005</b> - 
+			
+			No change required for MAME v0.94.
+		</p>
+    <p>
+      <b>27 February 2005</b> - 
+			
+			No change required for MAME v0.93.
+		</p>
+    <p>
+      <b>13 February 2005</b> - 
+			
+			Updated supplements for MAME v0.92.
+		</p>
+    <p>
+      <b>30 January 2005</b> - 
+			
+			Updated supplements for MAME v0.91.
+		</p>
+    <p>
+      <b>4 January 2005</b> - 
+			
+			Updated supplements for MAME v0.90.
+		</p>
+    <p>
+      <b>24 November 2004</b> - 
+			
+			No change required for MAME v0.89.
+		</p>
+    <p>
+      <b>25 October 2004</b> - 
+			
+			Updated supplements for MAME v0.88.
+		</p>
+    <p>
+      <b>23 September 2004</b> - 
+			
+			Updated supplements for MAME v0.87.
+		</p>
+    <p>
+      <b>24 August 2004</b> - 
+			
+			No change required for MAME v0.86.
+		</p>
+    <p>
+      <b>16 August 2004</b> - 
+			
+			Updated supplements for MAME v0.85.
+		</p>
+    <p>
+      <b>17 July 2004</b> - 
+			
+			The data file is now genuinely for Nebula Jukebox and also includes MAME supplements.
+		</p>
+    <p>
+      <b>11 July 2004</b> - 
+			
+			Added SHA1 information.
+		</p>
+    <p>
+      <b>7 March 2004</b> - 
+			
+			Updated to match MAME v0.80.
+		</p>
+    <p>
+      <b>1 February 2004</b> - 
+			
+			Updated using the latest Neo-Geo, CPS-2 and ZN data files.
+		</p>
+    <p>
+      <b>25 August 2003</b> - 
+			
+			Updated to match MAME v0.72.
+		</p>
+    <p>
+      <b>5 July 2003</b> - 
+			
+			Updated to match MAME v0.71.
+		</p>
+    <p>
+      <b>6 March 2003</b> - 
+			
+			Updated for Jukebox v2.8 (added fvipers and more ROMs for stcc).
+		</p>
+    <p>
+      <b>21 February 2003</b> - 
+			
+			Updated the Sega Model 2 games (plus 7 added).
+		</p>
+    <p>
+      <b>2 February 2003</b> - 
+			
+			Added some of the new games for v2.7.
+		</p>
+    <p>
+      <b>18 January 2003</b> - 
+			
+			Updated to match MAME v0.63.
+		</p>
+    <p>
+      <b>9 December 2002</b> - 
+			
+			Added Sega Model 2 games for Nebula Jukebox v2.5.
+		</p>
+    <p>
+      <b>5 December 2002</b> - 
+			
+			Added 1944 and changed batcira->batcir.
+		</p>
+    <p>
+      <b>30 November 2002</b> - 
+			
+			Updated to match MAME v0.62.
+		</p>
+    <p>
+      <b>31 October 2002</b> - 
+			
+			Trivial cosmetic changes.
+		</p>
+    <p>
+      <b>7 October 2002</b> - 
+			
+			Added orlegend, kov, mslug4, neonopon, jgokushi, progear, puzloop2.<br/>Renamed gwingj->gwing, rckman2j->megaman2, kof2001b->kof2001.<br/>Fixed V2 ROM in Savage Reign.
+		</p>
+    <p>
+      <b>2 May 2002</b> - 
+			
+			Updated to match MAME v0.60.
+		</p>
+    <p>
+      <b>14 April 2002</b> - 
+			
+			Added Bang Bead and KOF2001.
+		</p>
+    <p>
+      <b>9 April 2002</b> - 
+			
+			Updated psyforce to match Zinc v0.8.
+		</p>
+    <p>
+      <b>4 April 2002</b> - 
+			
+			Updated for Nebula Jukebox v2.1 (added bucky and moo).
+		</p>
+    <p>
+      <b>25 March 2002</b> - 
+			
+			Updated to use standard ROM names (MAME v0.59 and Zinc v0.5).
+		</p>
+    <p>
+      <b>4 March 2002</b> - 
+			
+			Updated for Nebula Jukebox v2.0a.
+		</p>
+    <p>
+      <b>6 February 2002</b> - 
+			
+			Updated kof2000 to match MAME v0.58.
+		</p>
+    <p>
+      <b>2 January 2002</b> - 
+			
+			Updated kof2000 and mslug3.
+		</p>
+    <p>
+      <b>9 December 2001</b> - 
+			
+			Changed v2 in mslug3.
+		</p>
+    <p>
+      <b>11 October 2001</b> - 
+			
+			Updated for Nebula Jukebox v1.62 (added nitd, s1945p and sengoku3).<br/>Changed kof99, kof2000, alpham2 and ganryu to use standard ROM names.
+		</p>
+    <p>
+      <b>10 October 2001</b> - 
+			
+			Updated for Nebula Jukebox v1.61 (added mslug3 and rckmanj).
+		</p>
+    <p>
+      <b>3 October 2001</b> - 
+			
+			First release.
+		</p>
+<script language="php">
+					// Standard page footer (counter)
+
+					include('../../Resources/Bottom.php');
+				</script>
+  </body>
+</html>
+
+
