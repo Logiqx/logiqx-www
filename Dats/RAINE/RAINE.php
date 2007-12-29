b4b2e582643145b8f34f040ab3140789e528ea23
Logiqx <software@mikeg.me.uk>
2007-12-29 21:09:52 +0000

    RAINE v0.50.6 is unchanged

--- Dats/RAINE/RAINE.php
+++ Dats/RAINE/RAINE.php
@@ -30,7 +30,7 @@
 	<p>
       <img src="RAINE.png" width="82" height="40" alt="RAINE.png"/>
     </p>
-	<p>577 games for RAINE v0.50.5</p>
+	<p>577 games for RAINE v0.50.5<br/>(suitable for v0.50.6)</p>
 	
 	
 	
