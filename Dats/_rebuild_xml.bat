a6e199ec3306857beae754174243238d47a0f106
Logiqx <software@mikeg.me.uk>
2008-02-05 17:56:37 +0000

    Fixed up for new laptop E:

--- Dats/_rebuild_xml.bat
+++ Dats/_rebuild_xml.bat
@@ -1,22 +1,22 @@
 @ECHO OFF
 
 ECHO Generating RecentEmus.xml...
-e:\tools\cmpro\cmpro /xml www.logiqx.com/Dats H:\Logiqx\WWW\Dats\RecentEmus.txt H:\Logiqx\WWW\Dats\RecentEmus.xml 
+e:\tools\clrmamepro\cmpro /xml www.logiqx.com/Dats e:\Logiqx\WWW\Dats\RecentEmus.txt e:\Logiqx\WWW\Dats\RecentEmus.xml 
 zip RecentEmus.zip RecentEmus.xml
 rm RecentEmus.xml
 
 ECHO Generating OlderEmus.xml...
-e:\tools\cmpro\cmpro /xml www.logiqx.com/Dats H:\Logiqx\WWW\Dats\OlderEmus.txt H:\Logiqx\WWW\Dats\OlderEmus.xml
+e:\tools\clrmamepro\cmpro /xml www.logiqx.com/Dats e:\Logiqx\WWW\Dats\OlderEmus.txt e:\Logiqx\WWW\Dats\OlderEmus.xml
 zip OlderEmus.zip OlderEmus.xml
 rm OlderEmus.xml
 
 ECHO Generating Systems.xml...
-e:\tools\cmpro\cmpro /xml www.logiqx.com/Dats H:\Logiqx\WWW\Dats\Systems.txt H:\Logiqx\WWW\Dats\Systems.xml
+e:\tools\clrmamepro\cmpro /xml www.logiqx.com/Dats e:\Logiqx\WWW\Dats\Systems.txt e:\Logiqx\WWW\Dats\Systems.xml
 zip Systems.zip Systems.xml
 rm Systems.xml
 
 ECHO Generating MAMESuperSet.xml...
-e:\tools\cmpro\cmpro /xml www.logiqx.com/Dats H:\Logiqx\WWW\Dats\MAMESuperSet.txt H:\Logiqx\WWW\Dats\MAMESuperSet.xml
+e:\tools\clrmamepro\cmpro /xml www.logiqx.com/Dats e:\Logiqx\WWW\Dats\MAMESuperSet.txt e:\Logiqx\WWW\Dats\MAMESuperSet.xml
 zip MAMESuperSet.zip MAMESuperSet.xml
 rm MAMESuperSet.xml
 
