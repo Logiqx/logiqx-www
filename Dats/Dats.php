92d9ab2f0be11e3e9d08ec95aef320d8ba073517
Logiqx <software@mikeg.me.uk>
2008-07-28 21:30:30 +0000

    FBA v0.2.96.88

--- Dats/Dats.php
+++ Dats/Dats.php
@@ -150,7 +150,7 @@ and I'll issue them to Roman (CMPro) and Eric (ROMCenter), ensuring that they wo
         </td>
         <td>
           <p>
-            <b>16/07/2008</b>
+            <b>28/07/2008</b>
           </p>
         </td>
         <td>
@@ -171,7 +171,7 @@ and I'll issue them to Roman (CMPro) and Eric (ROMCenter), ensuring that they wo
         </td>
         <td>
           <p>
-            <b>16/07/2008</b>
+            <b>28/07/2008</b>
           </p>
         </td>
         <td>
@@ -282,7 +282,7 @@ and I'll issue them to Roman (CMPro) and Eric (ROMCenter), ensuring that they wo
 
 			<tr>
         <td>
-          <a href="FinalBurnAlpha/">FBA v0.2.96.86</a>
+          <a href="FinalBurnAlpha/">FBA v0.2.96.88</a>
         </td>
         <td>
           <p>
@@ -291,12 +291,12 @@ and I'll issue them to Roman (CMPro) and Eric (ROMCenter), ensuring that they wo
         </td>
         <td>
           <p>
-            <b>16/07/2008</b>
+            <b>28/07/2008</b>
           </p>
         </td>
         <td>
           <p>
-            <b>1377</b>
+            <b>1384</b>
           </p>
         </td>
       </tr>
--- Dats/FinalBurnAlpha/FinalBurnAlpha.php
+++ Dats/FinalBurnAlpha/FinalBurnAlpha.php
@@ -8,7 +8,7 @@
 					include ('../../Resources/Include.php');
 
 				</script>
-    <title>FBA v0.2.96.86</title>
+    <title>FBA v0.2.96.88</title>
     <script language="php">
 					// Include standard &lt;head> metadata
 
@@ -29,7 +29,7 @@
 	<p>
       <img src="FinalBurnAlpha.png" width="195" height="40" alt="FinalBurnAlpha.png"/>
     </p>
-	<p>1377 games for FBA v0.2.96.86</p>
+	<p>1384 games for FBA v0.2.96.88</p>
 	
 	
 	
@@ -41,17 +41,17 @@
       <colgroup span="1" width="50%"/>
       <tr>
 		<td>
-          <p>Download <a href="FBA%20v0.2.96.86%20(xml).zip">FBA dat for CMPro + RC3</a> (498KB)</p>
+          <p>Download <a href="FBA%20v0.2.96.88%20(xml).zip">FBA dat for CMPro + RC3</a> (499KB)</p>
           <p>
-            <a href="FBA%20v0.2.96.86%20(xml).zip">
+            <a href="FBA%20v0.2.96.88%20(xml).zip">
               <img src="../../Resources/Disk.png" width="38" height="38" alt="Download"/>
             </a>
           </p>
         </td>
 		<td>
-          <p>Download <a href="FBA%20v0.2.96.86%20(rc).zip">FBA dat for ROMCenter 2</a> (244KB)</p>
+          <p>Download <a href="FBA%20v0.2.96.88%20(rc).zip">FBA dat for ROMCenter 2</a> (245KB)</p>
           <p>
-            <a href="FBA%20v0.2.96.86%20(rc).zip">
+            <a href="FBA%20v0.2.96.88%20(rc).zip">
               <img src="../../Resources/Disk.png" width="38" height="38" alt="Download"/>
             </a>
           </p>
@@ -62,6 +62,11 @@
     <p>
       <img src="../../Resources/History.png" width="85" height="40" alt="History"/>
     </p>
+    <p>
+      <b>28 July 2008</b> - 
+			
+			Updated for FBA v0.2.96.88.
+		</p>
     <p>
       <b>16 July 2008</b> - 
 			
