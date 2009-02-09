06d850b858dd2ff4c588299ef5bc28c99fa050bd
Logiqx <software@mikeg.me.uk>
2009-02-09 08:39:35 +0000

    Fix wildcard expression

--- Dats/MAMESuperSetSuppM/MAMESuperSetSuppMHistory.php
+++ Dats/MAMESuperSetSuppM/MAMESuperSetSuppMHistory.php
@@ -32,7 +32,7 @@
 						<tr>
 							<?php
 								echo '<td><p>' . LF;
-								foreach (glob("*Supplements*(*m*).zip") as $filename)
+								foreach (glob("*Supplement*(*m*).zip") as $filename)
 								{
    									echo INDENT . TAB . TAB . TAB . '<a href="' . $filename . '">' . $filename . '</a>';
 									echo ' (' . ceil(filesize($filename)/1024) . 'KB)<br />' . LF;
@@ -40,7 +40,7 @@
 								echo INDENT . TAB . TAB . '</p></td>' . LF;
 
 								echo '<td><p>' . LF;
-								foreach (glob("*Supplements*(rc).zip") as $filename)
+								foreach (glob("*Supplement*(rc).zip") as $filename)
 								{
    									echo INDENT . TAB . TAB . TAB . '<a href="' . $filename . '">' . $filename . '</a>';
 									echo ' (' . ceil(filesize($filename)/1024) . 'KB)<br />' . LF;
